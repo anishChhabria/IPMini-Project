110d4e51231eb191191466287719d9ddfa1854cd
Anshul Chaudary <anhulchaudhary.218@gmail.com>
2019-09-24 09:52:39 +0530

    navbar not complete

--- resources/views/navbar/navs1.blade.php
+++ resources/views/navbar/navs1.blade.php
@@ -1,23 +1,79 @@
-<div class="container">
-    <nav id="topNav" class="navbar navbar-default navbar-fixed-top">
+<div>
+    for best view try to access the website on laptop/desktop
+</div>
+<nav id="topNav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
         <div id="logo" class="col-4">
             LOGO
         </div>
-        <!-- <div id="search" class="col-6">
-            <input class = "searchBar" type="text" placeholder="Search..." name="search">
-            <button class = "searchButton" type="submit"><i class="fa fa-search"></i></button>
-        </div> -->
-        <div id="search" class="col-6 input-group md-form form-sm form-2 pl-0">
-            <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search..." aria-label="Search">
-            <div class="input-group-append">
-                <span class="input-group-text red lighten-3" id="basic-text1"><i class="fas fa-search text-grey"
-                aria-hidden="true"></i></span>
-            </div>  
+        <div id="search" class="col-6 input-group md-form form-sm form-2 pl-0 ">
+            <input id = "searchBar" class="form-control my-0 py-1 red-border" type="text" placeholder="Search..." aria-label="Search">
+            <button id="searchButton"><i class="fas fa-search text-grey" aria-hidden="true"></i></button>
         </div>
         <div id="userProfile" class="col-2">
-            hello
+        <p class="navbar-text navbar-right actions">
+            <a class="plain" href="#">Log In</a>
+            
+        </p>
         </div>
-    </nav>
+    </div>
 
-
-</div>
+    <div class="container">
+        <div class="col-1.5 hello">
+            <a class="plain active" href="#"> 
+                Shop by <br> 
+                Category
+            </a>
+        </div>
+        <div class="col-1.5">
+            <a class="plain" href="#">
+                Shop by <br>
+                Brand
+            </a>
+        </div>
+        <div class="col-1.5">
+            <a class="plain" href="#">
+                Pre-built <br>
+                Computer
+            </a>
+        </div>
+        <div class="col-1.5">
+            <a class="plain" href="#">
+                Built your <br>
+                Computer
+            </a>
+        </div>
+        <div class="col-1.5">
+            <a class="plain" href="#">
+                Monitor
+            </a>
+        </div>
+        <div class="col-1.5">
+            <a class="plain" href="#">
+                Accessories
+            </a>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-0.5">
+            <a class="plain" href="#">
+                Compare
+            </a>
+        </div>
+        <div class="col-0.5">
+            <a class="plain" href="#">
+                    Offer
+            </a>
+        </div>
+        <div class="col-0.5">
+            <a class="plain wishlist" href="#">
+                <i class="fa fa-heart" aria-hidden="true"></i>
+            </a>
+        </div>
+        <div class="col-0.5">
+            <a class="plain cart" href="#">
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+            </a>
+        </div>
+        </div>
+    </div>
+</nav>
